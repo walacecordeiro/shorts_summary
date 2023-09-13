17e446942e71c6a1db52d8bf48265150c136f21c
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-09-13 15:49:08 -0300

    criei a rota para o servidor usando o método HTTP GET

--- server/index.js
+++ server/index.js
@@ -4,4 +4,8 @@ import express from "express";
 const app = express();
 app.use(cors());
 
+app.get("/resumo", (request, response) => {
+  response.send("Servidor online com a nova flag do node");
+});
+
 app.listen(3333, () => console.log(`O servidor está executando na porta 3333`));
